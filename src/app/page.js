12c821b8eb7fa19ae5fdf0267812d72b262b25da
fardinzam <fardinzaman51@gmail.com>
2024-01-03 22:33:40 -0800

    added colors, font, projects/details pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,7 @@
-import Navbar from "@/components/Navbar";
-import Welcome from "@/components/landing/Welcome";
-import About from "@/components/landing/About";
-import ProjectTypes from "@/components/landing/ProjectType";
-import PastProjects from "@/components/landing/PastProjects";
-import ViewMore from "@/components/ViewMore";
-import Footer from "@/components/Footer";
+import Landing from "@/components/landing/Landing";
 
 const Home = () => {
-  return (
-    <div className="w-full bg-blue-50 flex flex-col">
-      <Navbar />
-      <Welcome />
-      <About />
-      <ProjectTypes />
-      <PastProjects />
-      <ViewMore />
-      <Footer />
-    </div>
-  );
+  return <Landing />;
 };
 
 export default Home;
