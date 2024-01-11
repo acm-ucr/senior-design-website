b37aef2dc5733aad6275c336c4fdca65f6e511b6
Brian Byun <brianubyun@gmail.com>
2024-01-10 23:48:45 -0800

    Added Footer Component
    - Modified src/components/Footer.jsx to have similar design to what was
      provided
    - Ran npm run format to have organized and readable code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,42 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div
+      className="Footer"
+      style={{ textAlign: "center", background: "#daf0ff" }}
+    >
+      <div
+        className="UCR"
+        style={{
+          fontSize: "28px",
+          fontWeight: "bold",
+          color: "orange",
+          display: "inline",
+        }}
+      >
+        UCR{" "}
+      </div>
+      <div
+        className="Senior Designs"
+        style={{
+          fontSize: "28px",
+          fontWeight: "bold",
+          display: "inline",
+          margin: "0.25px",
+        }}
+      >
+        Senior Designs{" "}
+      </div>
+      <div
+        className="Copyright"
+        style={{
+          fontSize: "12px",
+          margin: "0.25px",
+        }}
+      >
+        Copyright © ACM UCR - All Rights Reserved.{" "}
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
