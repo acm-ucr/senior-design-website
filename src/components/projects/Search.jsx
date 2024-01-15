8263928fdbd73d1a45c826c5dadfbad57952ab70
Kevin Loritsch <kloritsch58@gmail.com>
2024-01-15 14:38:43 -0800

    Kevinloritsch/search

--- src/components/projects/Search.jsx
+++ src/components/projects/Search.jsx
@@ -1,5 +1,31 @@
-const Search = () => {
-  return <div>Search</div>;
-};
+"use client"; // This is a client component
 
+import { useState } from "react";
+
+const Search = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputValue);
+  };
+  return (
+    <div className="flex justify-center">
+      <form className="w-3/4" onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="search"
+            placeholder="Project Name"
+            className="rounded-lg w-full px-4"
+            value={inputValue}
+            onChange={handleChange}
+          />
+        </label>
+      </form>
+    </div>
+  );
+};
 export default Search;
