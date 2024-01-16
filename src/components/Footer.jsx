3d60072784c54a560bbaa21e40f282d060136f45
Brian Byun <brianubyun@gmail.com>
2024-01-15 18:20:21 -0800

    Changed background color and text color of Senior Designs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 const Footer = () => {
   return (
-    <div className="text-center bg-[#daf0ff] m-5">
+    <div className="text-center bg-[#eaf5f8] m-5">
       <p className="mt-4 text-yellow-400 font-bold text-2xl inline-block">
         {" "}
         UCR
       </p>
-      <p className="inline-block ml-2 font-bold text-2xl text-gray-700">
+      <p className="inline-block ml-2 font-bold text-2xl">
         Senior Designs
       </p>
       <p className="text-xs mb-12">
