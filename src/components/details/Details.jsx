25650119c315731eb996a09a8df048f39777ff5e
Brian Byun <brianubyun@gmail.com>
2024-01-24 15:26:01 -0800

    Added temporary thumbnail for projects details page

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -1,5 +1,13 @@
 const Details = () => {
-  return <div>Details</div>;
+  return (
+    <div>
+      <img
+        className="object-cover h-96 w-full "
+        src="/image1.png"
+        alt="Thumbnail for project"
+      ></img>
+    </div>
+  );
 };
 
 export default Details;
