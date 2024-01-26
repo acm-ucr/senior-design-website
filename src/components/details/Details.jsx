b3441b756ca6c4db93863fe908a4f5216eba2e95
Stanley Lew <stanley43595@gmail.com>
2024-01-26 13:08:05 -0800

    formatting

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -6,9 +6,9 @@ const Details = () => {
     <div className="bg-white px-8 md:px-64">
       <div className="flex items-center gap-1">
         <div className="bg-senior-yellow-100 h-5 w-1" />
-        <div className="font-bold">Inspiration</div>
+        <p className="font-bold text-lg pt-3">Inspiration</p>
       </div>
-      <div className="pl-4 pt-4 pb-12">
+      <p className="pl-4 pt-4 pb-12">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
@@ -16,14 +16,14 @@ const Details = () => {
         velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
         occaecat cupidatat non proident, sunt in culpa qui officia deserunt
         mollit anim id est laborum.
-      </div>
+      </p>
 
       <div className="flex items-center gap-1">
         <div className="bg-senior-yellow-100 h-5 w-1" />
-        <div className="font-bold">Purpose</div>
+        <p className="font-bold text-lg pt-3">Purpose</p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 pl-4 pt-4 pb-12 gap-4">
-        <div>
+        <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliqu
@@ -32,7 +32,7 @@ const Details = () => {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliqu
-        </div>
+        </p>
         <div>
           <Image src={img1.src} width={350} height={250} alt="projectimage" />
         </div>
@@ -40,7 +40,7 @@ const Details = () => {
 
       <div className="flex items-center gap-1">
         <div className="bg-senior-yellow-100 h-5 w-1" />
-        <div className="font-bold">Video</div>
+        <p className="font-bold text-lg pt-3">Video</p>
       </div>
 
       <div className="pl-4 pt-4 pb-16 flex justify-center">
