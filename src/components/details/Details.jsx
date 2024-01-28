0bf21111767746ff194d0839e0d85aec47bad455
Brian Byun <brianubyun@gmail.com>
2024-01-28 13:55:52 -0800

    Fixed formatting of thumbnail

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -1,57 +1,67 @@
 import img1 from "../../../public/tempimage.png";
+import img2 from "../../../public/image1.png";
 import Image from "next/image";
 
 const Details = () => {
   return (
-    <div className="bg-white px-8 md:px-64">
-      <img
-        className="object-cover h-96 w-full "
-        src="/image1.png"
-        alt="Thumbnail for project"
-      ></img>
-      <div className="flex items-center gap-1">
-        <div className="bg-senior-yellow-100 h-5 w-1" />
-        <p className="font-bold text-lg pt-3">Inspiration</p>
-      </div>
-      <p className="pl-4 pt-4 pb-12">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-
-      <div className="flex items-center gap-1">
-        <div className="bg-senior-yellow-100 h-5 w-1" />
-        <p className="font-bold text-lg pt-3">Purpose</p>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 pl-4 pt-4 pb-12 gap-4">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliqu
-          <br />
-          <br />
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliqu
-        </p>
-        <div>
-          <Image src={img1.src} width={350} height={250} alt="projectimage" />
+    <>
+      <div className="bg-white px-0 md:px-0">
+        <div className="relative h-96 w-full">
+          <Image
+            src={img2.src}
+            layout="fill"
+            objectFit="cover"
+            alt="projectImage"
+          />
         </div>
       </div>
+      <div className="bg-white px-8 md:px-64">
+        <div className="flex items-center gap-1">
+          <div className="bg-senior-yellow-100 h-5 w-1" />
+          <p className="font-bold text-lg pt-3">Inspiration</p>
+        </div>
+        <p className="pl-4 pt-4 pb-12">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
 
-      <div className="flex items-center gap-1">
-        <div className="bg-senior-yellow-100 h-5 w-1" />
-        <p className="font-bold text-lg pt-3">Video</p>
-      </div>
+        <div className="flex items-center gap-1">
+          <div className="bg-senior-yellow-100 h-5 w-1" />
+          <p className="font-bold text-lg pt-3">Purpose</p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 pl-4 pt-4 pb-12 gap-4">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliqu
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliqu
+          </p>
+          <div>
+            <Image src={img1.src} width={350} height={250} alt="projectimage" />
+          </div>
+        </div>
 
-      <div className="pl-4 pt-4 pb-16 flex justify-center">
-        <Image src={img1.src} width={450} height={350} alt="projectimage" />
+        <div className="flex items-center gap-1">
+          <div className="bg-senior-yellow-100 h-5 w-1" />
+          <p className="font-bold text-lg pt-3">Video</p>
+        </div>
+
+        <div className="pl-4 pt-4 pb-16 flex justify-center">
+          <Image src={img1.src} width={450} height={350} alt="projectimage" />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
