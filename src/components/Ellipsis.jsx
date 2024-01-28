4f7d433f0f5138950fdcb0b9ce554f7bd03ce804
michellesun2 <122694710+michellesun2@users.noreply.github.com>
2024-01-28 15:34:35 -0800

    added circle and color

--- src/components/Ellipsis.jsx
+++ src/components/Ellipsis.jsx
@@ -1,5 +1,11 @@
+import { VscEllipsis } from "react-icons/vsc";
+
 const Ellipsis = () => {
-  return <>ellipsis icon</>;
+  return (
+    <button className="rounded-full bg-senior-yellow-100 w-5">
+      <VscEllipsis />
+    </button>
+  );
 };
 
 export default Ellipsis;
