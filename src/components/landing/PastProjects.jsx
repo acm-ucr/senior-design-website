8ba7a6c98c5968abfd967a38166c6805ac126930
billblaster <102612368+billblaster@users.noreply.github.com>
2024-01-29 13:08:08 -0800

    making flex rows

--- src/components/landing/PastProjects.jsx
+++ src/components/landing/PastProjects.jsx
@@ -1,5 +1,19 @@
+import React from "react";
+import FakeData from "@/data/fakedata";
+import ProjectCard from "@/components/projectcard";
+
 const PastProjects = () => {
-  return <div>2022 - 2023</div>;
+  return (
+    <div className="bg-white  flex justify-center items-center text-center flex-col">
+      <div>2022 - 2023</div>
+      <div className="flex flex-wrap justify-around" >
+        {FakeData.map((project, index) => (
+          <ProjectCard key={index} project={project} />
+        ))}
+      </div>
+      <button className="bg-yellow-300 ">Load More</button>
+    </div>
+  );
 };
 
 export default PastProjects;
