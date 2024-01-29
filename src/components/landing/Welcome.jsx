691254abd0e1ef26b776a9c86d0d7ecb8705ab06
michellesun2 <122694710+michellesun2@users.noreply.github.com>
2024-01-29 12:08:05 -0800

    image is at 100% size

--- src/components/landing/Welcome.jsx
+++ src/components/landing/Welcome.jsx
@@ -1,11 +1,8 @@
+import Image from "next/image";
+import Bourns_Building from "@/public/Bourns_Building.png";
 const Welcome = () => {
   return (
-    <img
-      src="Bourns_Building.png"
-      alt="Image of Bourns"
-      width="100%"
-      height="100%"
-    />
+    <Image src={Bourns_Building} width="100%" height="100%" alt="bourns building" />
   );
 };
 
