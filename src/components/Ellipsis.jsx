2371590c58afd6019cd6a8efbcef68cf6c740ca1
michellesun2 <122694710+michellesun2@users.noreply.github.com>
2024-02-07 23:54:36 -0800

    unable to use classname= inside of react icon

--- src/components/Ellipsis.jsx
+++ src/components/Ellipsis.jsx
@@ -2,8 +2,8 @@ import { IoEllipsisHorizontalSharp } from "react-icons/io5";
 
 const Ellipsis = () => {
   return (
-    <button className="rounded-full bg-senior-yellow-100 w-4 h-4">
-      <IoEllipsisHorizontalSharp color="white" />
+    <button className="rounded-full bg-senior-yellow-100 w-9 h-15">
+      <IoEllipsisHorizontalSharp color="white" size="35" />
     </button>
   );
 };
