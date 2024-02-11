9ee8187e6643029c56165795c39bce80e082d904
Brian Byun <brianubyun@gmail.com>
2024-02-11 13:24:02 -0800

    Moved Footer component from landing to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,22 @@ const raleway = Raleway({
   variable: "--font-nunito",
 });
 
+// eslint-disable-next-line no-unused-vars
+const Footer = () => {
+  return (
+    <div className="text-center bg-[#eaf5f8] m-5">
+      <p className="mt-4 text-yellow-400 font-bold text-2xl inline-block">
+        {" "}
+        UCR
+      </p>
+      <p className="inline-block ml-2 font-bold text-2xl">Senior Designs</p>
+      <p className="text-xs mb-12">
+        Copyright © ACM UCR - All Rights Reserved.
+      </p>
+    </div>
+  );
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full">
