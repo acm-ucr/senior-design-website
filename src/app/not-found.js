e22286a6b0080283a7329cba65c26b42a1b46e63
John Zhou <zhou9865@gmail.com>
2024-02-12 18:11:32 -0800

    margin too far down so I added negative padding

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 const NotFound = () => {
   return (
-    <div class="flex flex-col items-center justify-center min-h-screen ">
+    <div class="flex flex-col items-center justify-center min-h-screen -mb-40 -mt-10">
       <h1 class="text-7xl font-bold text-error-teal">404</h1>
       <h2 class="text-2xl font-bold">Page not found</h2>
 
