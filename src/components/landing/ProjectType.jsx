a69d1cf5c79e243545b971368394e4fe8d87b405
Brian Byun <brianubyun@gmail.com>
2024-02-17 23:31:45 -0800

    Current state of ProjectType code

--- src/components/landing/ProjectType.jsx
+++ src/components/landing/ProjectType.jsx
@@ -11,15 +11,15 @@ import Link from "next/link";
 
 const ProjectTypes = () => {
   return (
-    <div className="bg-white py-12">
-      <p className="text-senior-purple-100 text-center font-bold text-3xl pt-10 pb-16">
+    <div className="bg-white">
+      <p className="text-senior-purple-100 text-center font-bold text-3xl pt-10 pb-12">
         Types of Projects
       </p>
       <div className="flex justify-center">
-        <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-14 px-20">
+        <div className="grid xl:grid-cols-4 md:grid-cols-2 gap-12">
           <Link
             href="/projects"
-            className="bg-projects-emb-100 rounded-xl relative overflow-hidden flex p-3 min-w-40 max-w-40 h-40"
+            className="bg-projects-emb-100 rounded-xl relative overflow-hidden flex p-3 min-w-60 max-w-60 h-60"
           >
             <Image
               src={emblogo.src}
@@ -28,7 +28,7 @@ const ProjectTypes = () => {
               alt="emblogo"
               className="-ml-12 md:-ml-12"
             />
-            <p className="text-white text-xl font-bold text-right mr-2 absolute inset-x-0 bottom-0">
+            <p className="text-white text-2xl font-bold text-right mr-2 absolute inset-x-0 bottom-0">
               EMBEDDED
               <br />
               SYSTEMS
@@ -36,7 +36,7 @@ const ProjectTypes = () => {
           </Link>
           <Link
             href="/projects"
-            className="bg-projects-database-100 rounded-xl relative overflow-hidden flex p-3 min-w-40 max-w-40 h-40"
+            className="bg-projects-database-100 rounded-xl relative overflow-hidden flex p-3 min-w-60 max-w-60 h-60 "
           >
             <Image
               src={databaselogo.src}
@@ -45,13 +45,13 @@ const ProjectTypes = () => {
               alt="databaselogo"
               className="-ml-8 md:-ml-8"
             />
-            <p className="text-white text-xl font-bold text-right mr-2 absolute inset-x-0 bottom-0">
+            <p className="text-white text-2xl font-bold text-right mr-2 absolute inset-x-0 bottom-0">
               DATABASE
             </p>
           </Link>
           <Link
             href="/projects"
-            className="bg-projects-network-100 rounded-xl relative overflow-hidden flex p-3 min-w-40 max-w-40 h-40"
+            className="bg-projects-network-100 rounded-xl relative overflow-hidden flex p-3 min-w-60 max-w-60 h-60"
           >
             <Image
               src={networklogo.src}
@@ -60,13 +60,13 @@ const ProjectTypes = () => {
               alt="networklogo"
               className="-ml-12 md:-ml-10"
             />
-            <p className="text-white text-xl font-bold text-right mr-2 absolute inset-x-0 bottom-0">
+            <p className="text-white text-2xl font-bold text-right mr-2 absolute inset-x-0 bottom-0">
               NETWORK
             </p>
           </Link>
           <Link
             href="/projects"
-            className="bg-projects-game-100 rounded-xl relative overflow-hidden flex p-2 min-w-40 max-w-40 h-40"
+            className="bg-projects-game-100 rounded-xl relative overflow-hidden flex p-2 min-w-60 max-w-60 h-60"
           >
             <Image
               src={electroniclogo.src}
@@ -75,7 +75,7 @@ const ProjectTypes = () => {
               alt="electroniclogo"
               className="-ml-6 md:-ml-6"
             />
-            <p className="text-white text-xl font-bold text-right mr-2 absolute inset-x-0 bottom-0">
+            <p className="text-white text-2xl font-bold text-right mr-2 absolute inset-x-0 bottom-0">
               ELECTRONIC
               <br />
               GAME
@@ -83,7 +83,7 @@ const ProjectTypes = () => {
           </Link>
           <Link
             href="/projects"
-            className="bg-projects-compiler-100 rounded-xl relative overflow-hidden flex p-3 min-w-40 max-w-40 h-40"
+            className="bg-projects-compiler-100 rounded-xl relative overflow-hidden flex p-3 min-w-60 max-w-60 h-60"
           >
             <Image
               src={compilerlogo.src}
@@ -92,13 +92,13 @@ const ProjectTypes = () => {
               alt="compilerlogo"
               className="-ml-8 -mb-6 md:-ml-8 md:-mb-6"
             />
-            <p className="text-white text-xl font-bold text-right mr-2 absolute inset-x-0 bottom-0">
+            <p className="text-white text-2xl font-bold text-right mr-2 absolute inset-x-0 bottom-0">
               COMPILER
             </p>
           </Link>
           <Link
             href="/projects"
-            className="bg-projects-operating-100 rounded-xl relative overflow-hidden flex min-w-40 max-w-40 h-40"
+            className="bg-projects-operating-100 rounded-xl relative overflow-hidden flex min-w-60 max-w-60 h-60"
           >
             <Image
               src={oslogo.src}
@@ -107,7 +107,7 @@ const ProjectTypes = () => {
               alt="oslogo"
               className="-ml-8 md:-ml-8"
             />
-            <p className="text-white text-xl font-bold text-right mr-2 absolute inset-x-0 bottom-0">
+            <p className="text-white text-2xl font-bold text-right mr-2 absolute inset-x-0 bottom-0">
               OPERATING
               <br />
               SYSTEMS
@@ -115,7 +115,7 @@ const ProjectTypes = () => {
           </Link>
           <Link
             href="/projects"
-            className="bg-projects-ai-100 rounded-xl relative overflow-hidden flex p-2 min-w-40 max-w-40 h-40"
+            className="bg-projects-ai-100 rounded-xl relative overflow-hidden flex p-2 min-w-60 max-w-60 h-60"
           >
             <Image
               src={ailogo.src}
@@ -124,7 +124,7 @@ const ProjectTypes = () => {
               alt="ailogo"
               className="-ml-8 md:-ml-8"
             />
-            <p className="text-white text-xl font-bold text-right mr-2 absolute inset-x-0 bottom-0">
+            <p className="text-white text-2xl font-bold text-right mr-2 absolute inset-x-0 bottom-0">
               ARTIFICIAL
               <br />
               INTELLIGENCE
@@ -132,7 +132,7 @@ const ProjectTypes = () => {
           </Link>
           <Link
             href="/projects"
-            className="bg-projects-others-100 rounded-xl relative overflow-hidden flex p-2 min-w-40 max-w-40 h-40"
+            className="bg-projects-others-100 rounded-xl relative overflow-hidden flex p-2 min-w-60 max-w-60 h-60"
           >
             <Image
               src={otherslogo.src}
@@ -141,7 +141,7 @@ const ProjectTypes = () => {
               alt="otherslogo"
               className="-ml-10 -mb-6 md:-ml-10 md:-mb-6"
             />
-            <p className="text-white text-xl font-bold text-right mr-2 absolute inset-x-0 bottom-0">
+            <p className="text-white text-2xl font-bold text-right mr-2 absolute inset-x-0 bottom-0">
               OTHERS
             </p>
           </Link>
