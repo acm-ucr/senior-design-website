760a6acdbc370bcdf4242274bc61027437a23d5b
billblaster <102612368+billblaster@users.noreply.github.com>
2024-02-26 05:22:23 -0800

    fixed issues

--- src/components/landing/Welcome.jsx
+++ src/components/landing/Welcome.jsx
@@ -2,13 +2,16 @@ import Image from "next/image";
 import bournsBuilding from "@/public/Bourns_Building.png";
 const Welcome = () => {
   return (
-    <div className="flex items-center justify-center max-h-96 max-w-full">
-      <Image
-        src={bournsBuilding}
-        width="100%"
-        height="100%"
-        alt="bourns building"
-      />
+    <div className="flex items-center justify-center max-h-screen max-w-full">
+      <div className="max-h-min lg:-mt-96  lg:-mb-16">
+        <Image
+          src={bournsBuilding}
+          width="100%"
+          height="100%"
+          alt="bourns building"
+          objectFit="cover"
+        />
+      </div>
     </div>
   );
 };
