5b294543d6d941cc982c5f1a58a91656b0852a9e
RheaNiraj <rheaniraj123.com>
2024-02-26 14:22:34 -0800

    recomitting view more component

--- src/components/ViewMore.jsx
+++ src/components/ViewMore.jsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 
 const ViewMore = () => {
   return (
-    
     <div className="mx-auto w-35  text-center px-6 py-2.5 bg-yellow-400 font-bold text-xl rounded shadow-md hover:bg-yellow-500 hover:shadow-lg focus:outline-none focus:shadow-outline">
       {" "}
       <Link href="/projects" className="no-underline text-white">
