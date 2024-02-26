3bb2d91336624c25a3ba813f260b03d94cd5d8c5
RheaNiraj <rheaniraj123.com>
2024-02-25 21:33:11 -0800

    view more component

--- src/components/ViewMore.jsx
+++ src/components/ViewMore.jsx
@@ -1,5 +1,16 @@
+import React from "react";
+import Link from "next/link";
+
 const ViewMore = () => {
-  return <div>view more</div>;
+  return (
+    
+    <div className="mx-auto w-35  text-center px-6 py-2.5 bg-yellow-400 font-bold text-xl rounded shadow-md hover:bg-yellow-500 hover:shadow-lg focus:outline-none focus:shadow-outline">
+      {" "}
+      <Link href="/projects" className="no-underline text-white">
+        view more
+      </Link>
+    </div>
+  );
 };
 
 export default ViewMore;
