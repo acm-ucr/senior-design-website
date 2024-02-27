d1037d328c20fbdd4280037231725a0b70ede92a
fardinzam <fardinzaman51@gmail.com>
2024-02-27 00:57:51 -0800

    some small changes

--- src/components/ViewMore.jsx
+++ src/components/ViewMore.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const ViewMore = () => {
   return (
-    <div className="mx-auto w-35  text-center px-6 py-2.5 bg-yellow-400 font-bold text-xl rounded shadow-md hover:bg-yellow-500 hover:shadow-lg focus:outline-none focus:shadow-outline">
+    <div className="px-6 py-2.5 mx-auto w-35 mb-5 text-center text-xl  font-bold bg-yellow-400 hover:bg-yellow-500 rounded shadow-md hover:shadow-lg focus:outline-none focus:shadow-outline">
       {" "}
       <Link href="/projects" className="no-underline text-white">
         view more
