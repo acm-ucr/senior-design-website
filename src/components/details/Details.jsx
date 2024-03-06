3e5862b43c0573206fcdc4c2d9ac6904e697b555
Brian Byun <brianubyun@gmail.com>
2024-03-06 13:20:12 -0800

    Changed responsiveness of text box size and changed file to json

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -2,19 +2,19 @@ import Image from "next/image";
 import Title from "./Title";
 import img1 from "../../../public/tempimage.png";
 import img2 from "../../../public/image1.png";
-import FakeDetailsData from "@/data/fakedetailsdata";
+import FakeDetailsData from "@/data/fakedetailsdata.json";
 
 const Details = () => {
-  const inspiration = FakeDetailsData[0].inspiration;
-  const purpose = FakeDetailsData[0].purpose;
-  const video = FakeDetailsData[0].video;
+  const inspiration = FakeDetailsData.events[0].inspiration;
+  const purpose = FakeDetailsData.events[0].purpose;
+  const video = FakeDetailsData.events[0].video;
 
   return (
-    <div className="bg-white px-0 md:px-0">
-      <div className="relative h-96 w-full">
+    <div className="bg-white px-4 md:px-8 lg:px-16 xl:px-32">
+      <div className="relative h-96">
         <Image src={img2} layout="fill" objectFit="cover" alt="projectImage" />
       </div>
-      <div className="bg-white px-8 md:px-32 lg:px-32 xl:px-64">
+      <div className="bg-white pt-8 md:pt-12 lg:pt-16">
         <Title
           year="2023-2024"
           projectName="Project Name"
@@ -22,38 +22,31 @@ const Details = () => {
           type="Operating System"
           logo="oslogo"
         />
-
         <div className="flex items-center gap-1 pt-5">
           <div className="bg-senior-yellow-100 h-5 w-1 -mt-4" />
           <p className="font-bold text-lg">Inspiration</p>
         </div>
-
-        <div className="pl-4 pt-4 pb-12">
+        <div className="pl-4 pt-4 pb-8 md:pb-12 lg:pb-16">
           <div>{inspiration}</div>
         </div>
-
         <div className="flex items-center gap-1">
           <div className="bg-senior-yellow-100 h-5 w-1 -mt-4" />
           <p className="font-bold text-lg">Purpose</p>
         </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 pl-4 pt-4 pb-12 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 pl-4 pt-4 pb-8 md:pb-12 lg:pb-16">
           <div>
             <div>{purpose}</div>
           </div>
-
           <div>
             <Image src={img1} width={350} height={250} alt="projectimage" />
           </div>
         </div>
-
         <div className="flex items-center gap-1">
           <div className="bg-senior-yellow-100 h-5 w-1 mt-2" />
           <p className="font-bold text-lg pt-6">Video</p>
         </div>
-
-        <div className="text-center">{video}</div>
-        <div className="pl-4 pt-2 pb-16 flex justify-center">
+        <div className="text-center pb-8 md:pb-12 lg:pb-16">{video}</div>
+        <div className="flex justify-center">
           <Image src={img1} width={450} height={350} alt="projectimage" />
         </div>
       </div>
