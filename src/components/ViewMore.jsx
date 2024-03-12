4c9fcf7d8c32600184c8816f9a6e26724b39745a
isabellecelo <isabelle.celo04@gmail.com>
2024-03-12 02:02:25 -0700

    fixed view button

--- src/components/ViewMore.jsx
+++ src/components/ViewMore.jsx
@@ -3,10 +3,14 @@ import Link from "next/link";
 
 const ViewMore = () => {
   return (
-    <div className="px-6 py-2.5 mx-auto w-35 mb-5 text-center text-xl  font-bold bg-yellow-400 hover:bg-yellow-500 rounded shadow-md hover:shadow-lg focus:outline-none focus:shadow-outline">
-      {" "}
+    <div className="mx-auto w-35 mb-5 ">
       <Link href="/projects" className="no-underline text-white">
-        view more
+        <button
+          className="px-6 py-2.5 text-center text-xl  font-bold bg-yellow-400 hover:bg-yellow-500 
+                          rounded shadow-md hover:shadow-lg focus:outline-none focus:shadow-outline"
+        >
+          view more
+        </button>
       </Link>
     </div>
   );
