185e8c868823aeaa6699f8a97eaba64f688238cf
John Zhou <zhou9865@gmail.com>
2024-03-14 00:42:11 -0700

    fixed the build problem by taking out searchParams being passed into the page. Why tho???

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,12 +1,19 @@
 "use client";
 import Projects from "@/components/projects/Projects";
+import { useSearchParams } from "next/navigation";
 
-const Page = ({ searchParams }) => {
-  const searchContent = searchParams?.query || "";
+const Page = () => {
+  const searchParams = useSearchParams();
+
+  let searchContent = "";
   const years = [];
-  for (const x in searchParams) {
-    if (x == "query") continue;
-    years.push(x);
+  for (const entry of searchParams.entries()) {
+    console.log(entry, searchParams.get(entry));
+    if (entry[0] == "query") {
+      searchContent = entry[1];
+    } else {
+      years.push(entry[0]);
+    }
   }
   return <Projects searchContent={searchContent} chosenYears={years} />;
 };
