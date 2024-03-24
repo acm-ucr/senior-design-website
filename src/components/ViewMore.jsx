eaf5ca46b9d895f335ea173517c5eab5303c650f
fardinzam <fardinzaman51@gmail.com>
2024-03-24 11:29:15 -0700

    small change

--- src/components/ViewMore.jsx
+++ src/components/ViewMore.jsx
@@ -5,12 +5,12 @@ const ViewMore = () => {
   return (
     <div className="mx-auto w-35 mb-5 ">
       <Link href="/projects" className="no-underline text-white">
-        <button
+        <div
           className="px-6 py-2.5 text-center text-xl  font-bold bg-yellow-400 hover:bg-yellow-500 
                           rounded shadow-md hover:shadow-lg focus:outline-none focus:shadow-outline"
         >
           view more
-        </button>
+        </div>
       </Link>
     </div>
   );
