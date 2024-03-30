e0a4eefbaf2ff40eecbfca49d3f49ff81a6e7965
fardinzam <fardinzaman51@gmail.com>
2024-03-30 10:05:57 -0700

    added project entries, removed project types

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,10 @@
 const Footer = () => {
   return (
-    <div className="text-center bg-blue-100 p-4 flex flex-col items-center justify-center">
+    <div className="h-20 text-center bg-blue-100 p-4 flex flex-col items-center justify-center">
       <div className="inline font-bold text-2xl">
         <span className="text-yellow-400 mx-2">UCR</span>
-        Senior Designs
+        <span>Senior Designs</span>
       </div>
-      <p className="text-xs">Copyright © ACM UCR - All Rights Reserved.</p>
     </div>
   );
 };
