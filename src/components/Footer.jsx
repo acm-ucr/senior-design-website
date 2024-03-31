b69343b23dc04514b68f75861490d534277dd73e
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-03-30 22:25:48 -0700

    responsive updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="h-20 text-center bg-blue-100 p-4 flex flex-col items-center justify-center">
+    <div className="h-32 text-center bg-blue-100 p-4 flex flex-col items-center justify-center">
       <div className="inline font-bold text-2xl">
         <span className="text-yellow-400 mx-2">UCR</span>
         <span>Senior Designs</span>
--- src/components/landing/Welcome.jsx
+++ src/components/landing/Welcome.jsx
@@ -7,21 +7,25 @@ const Welcome = () => {
       <Image
         src={bournsBuilding}
         alt="Winston Chung Hall Patio"
-        className="bg-cover h-screen"
+        className="bg-cover object-cover h-screen"
       />
-      <div className="w-1/2 absolute left-10 top-1/2 -translate-y-1/2 bg-white bg-opacity-80 p-8 rounded">
-        <p className="font-bold lg:text-4xl md:text-2xl">UCR Senior Designs</p>
+      <div className="w-4/5 md:w-1/2 absolute left-10 top-1/2 -translate-y-1/2 bg-white bg-opacity-80 p-8 rounded">
+        <p className="flex justify-center md:justify-start font-bold lg:text-4xl md:text-2xl">
+          UCR Senior Designs
+        </p>
         <p className="lg:text-lg text-left">
           Explore a myriad of hands-on learning experiences in our Senior Design
           projects developing cutting-edge solutions for real-world industry
           problems.
         </p>
-        <Link
-          href="/projects"
-          className="bg-buttons-orange text-white font-bold rounded-md px-3 py-2 no-underline hover:opacity-75"
-        >
-          PROJECTS
-        </Link>
+        <div className="flex justify-center md:justify-start">
+          <Link
+            href="/projects"
+            className="bg-buttons-orange text-white font-bold rounded-md px-3 py-2 no-underline hover:opacity-75"
+          >
+            PROJECTS
+          </Link>
+        </div>
       </div>
     </>
   );
